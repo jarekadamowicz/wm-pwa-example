b7c8fd7a62f1f730781d634b4406efbf8b1fba1c
Jarek <44131870+jarekadamowicz@users.noreply.github.com>
2023-09-19 06:51:49 +0200

    add forced install prompt

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -1,6 +1,17 @@
+var deferredPrompt;
+
 if ("serviceWorker" in navigator) {
   console.log("serviceWorker is available");
+
   navigator.serviceWorker.register("/sw.js").then(() => {
     console.log("serviceWorker is registered...");
   });
 }
+
+window.addEventListener("beforeinstallprompt", (event) => {
+  console.log("beforeinstallprompt fired");
+
+  event.preventDefault();
+  deferredPrompt = event;
+  return false;
+});
